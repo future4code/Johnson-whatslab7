946dc8e6fe3355b43a851e81c3071e04712ff003
Jonathan <me.info.jonathan@gmail.com>
2021-07-24 08:36:08 -0300

    Modificacoes

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,56 +1,108 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import ListaChatItem from './components/ListaChatItem';
+import ChatWindow from './Components/ChatWindow';
+import ChatListItem from './Components/ChatListItem';
+import ChatIntro from './Components/ChatIntro';
 
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 
-export default () => {
 
-    const [listachat, setChatList] = useState([{},{},{},{}]);
+
+
+
+export default () => {
+  const [chatlist, setChatList] = useState([
+
+    {
+      chatId: 1,
+      title: 'Fulano de Tal',
+      image: 'https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png", alt="foto-usuário'
+    },
+    {
+      chatId: 2,
+      title: 'Beltrano',
+      image: 'https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png", alt="foto-usuário'
+    },
+    {
+      chatId: 3,
+      title: 'Jaspion',
+      image: 'https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png", alt="foto-usuário'
+    },
+    {
+      chatId: 4,
+      title: 'Jaspion',
+      image: 'https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png", alt="foto-usuário'
+    },
+
+
+
+  ]);
+
+  const [activeChat, setActiveChat] = useState({});
   return (
-      <div className="app-janela">
-          <div className="barra-lateral">
-              
-              
-              <header>
-                  <img className="header-avatar" src="https://riscosystem.bss.design/assets/img/user.png" alt="" />
-                  <div className="header-buttons">
-                      <div className="header-bnt">
-                          <DonutLargeIcon style={{color: '#919191'}} />
-                      </div>
-                      <div className="header-bnt">
-                          <ChatIcon style={{color: '#919191'}} />
-                      </div>
-                      <div className="header-bnt">
-                          <MoreVertIcon style={{color: '#919191'}} />
-                      </div>
-                  </div>
-              </header>
-              
-              
-              <div className="busca">
-                  <div className="busca-input">
-                    <SearchIcon fontSize="small" style={{color: '#919191'}}/>
-                    <input type="search" placeholder="Procurar ou comecar nova conversa" />
-                  </div>
-              </div>
-              
-              
-              <div className="listaChat">
-                  {ListaChatItem.map((item, key)=>(
-                      <ListaChatItem  
-                        key={key}
-                      />
-                  ))};
-              </div>
+    <div className="app-window">
+
+      <div className="sidebar">
+
+        <header>
+          <img className="header-avatar" src="https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png" alt="foto-usuário" />
+
+          <div className="header-buttons">
+
+            <div className="header-btn">
+              <DonutLargeIcon style={{ color: "#919191" }} />
+
+            </div>
+            <div className="header-btn">
+              <ChatIcon style={{ color: "#919191" }} />
+
+            </div>
+            <div className="header-btn">
+              <MoreVertIcon style={{ color: "#919191" }} />
+
+            </div>
+
           </div>
-          <div className="conteudo-area">
-              ...
+
+        </header>
+
+        <div className="search">
+          <div className="search-input">
+            <SearchIcon fontSize="small" style={{ color: "#919191" }} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
           </div>
+        </div>
+
+        <div className="chatlist">
+          {chatlist.map((item, key) => (
+            <ChatListItem
+              key={key}
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+
+              onClick={() => setActiveChat(chatlist[key])}
+
+            />
+
+          ))}
+
+
+        </div>
+
       </div>
+      <div className="contentarea">
+        {activeChat.chatId !== undefined &&
+          <ChatWindow />
+        }
+        {activeChat.chatId === undefined &&
+          <ChatIntro />
+        }
+
+      </div>
+    </div>
+
   );
 }
-
